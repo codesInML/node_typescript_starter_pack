1db765c72712d16c1683ea8d59cd13132acfbb1b
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2022-01-22 19:42:16 +0100

    created a node, typescript and express setup

---
+++ app.ts
@@ -0,0 +1,30 @@
+// configure the environment variables
+require("dotenv").config();
+
+global.__basedir = __dirname;
+
+// set the port
+const PORT = process.env.PORT;
+
+// for async errors
+import "express-async-errors";
+
+import express, { Application } from "express";
+
+// security packages
+import helmet from "helmet";
+const xss = require("xss-clean");
+import rateLimit from "express-rate-limit";
+
+// initialize the express app
+const app: Application = express();
+
+// security middlewares
+app.set("trust proxy", 1);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(xss());
+app.use(rateLimit({ windowMs: 60 * 1000, max: 60 }));
+
+export default app;
